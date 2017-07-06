fcd8bb72173ac30926977abba571b6a86a93cc83
Luis Vicente <langel.vicente@gmail.com>
2017-07-06 13:20:37 +0100

    Rust :: Solution for gigasecond (simplified)

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -1,11 +1,9 @@
 extern crate chrono;
 
-use chrono::{DateTime, UTC};
-use chrono::duration::Duration;
-use std::ops::Add;
+use chrono::*;
 
 const GIGASECOND: i64 = 1_000_000_000;
 
 pub fn after(start_date: DateTime<UTC>) -> DateTime<UTC> {
-    start_date.add(Duration::seconds(GIGASECOND))
+    start_date + Duration::seconds(GIGASECOND)
 }
