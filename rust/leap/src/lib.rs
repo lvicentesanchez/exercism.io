16eadcc571b0ee047f40928c472570321d2f3d56
Luis Vicente <langel.vicente@gmail.com>
2017-07-06 14:47:45 +0100

    Rust :: Solution for leap (using a match)

--- rust/leap/src/lib.rs
+++ rust/leap/src/lib.rs
@@ -1,10 +1,8 @@
 pub fn is_leap_year(year: i64) -> bool {
-    ( year % 400 == 0 ) || ( ( year % 4 == 0 ) && ( year % 100 != 0 ) )
-    /*
     match year {
         yyyy if yyyy % 400 == 0 => true,
         yyyy if yyyy % 100 == 0 => false,
         yyyy if yyyy % 4   == 0 => true,
         _                       => false
-    }*/
+    }
 }
